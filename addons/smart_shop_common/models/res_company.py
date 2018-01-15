05b339343e2705661e34db71ef9f74a824f66770
muhammad asim sajjad <a.sajjad@intagleo.co.uk>
2018-01-15 18:59:58 +0500

    deleted smart shop

--- addons/smart_shop_common/models/res_company.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields, _
-
-
-class ResCompany(models.Model):
-    _inherit = 'res.company'
-
-    opening_hours = fields.Char(string=_('Opening Hours'))
