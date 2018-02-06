a9eb904bd64cf3472a0331ce7b0dcafd5031d4ed
muhammad asim sajjad <a.sajjad@intagleo.co.uk>
2018-02-06 13:30:29 +0500

    removed shop modules

--- addons/smart_shop_client/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import product
-from . import store_config_settings
--- addons/smart_shop_create_store/static/src/js/custom-additional-methods.js
+++
@@ -1,3 +0,0 @@
-$.validator.addMethod("domain", function(value, element) {
-    return this.optional(element) || /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9]$/.test(value);
-}, "Please specify a valid domain name");
